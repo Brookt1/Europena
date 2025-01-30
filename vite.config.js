6077062c26fca80fabb5f6ebcf07552293420e42
BileyX <brxsitota@gmail.com>
2025-01-30 14:25:52 +0300

    Update deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: "/Europena/",  //  Must match your GitHub repo name exactly
   plugins: [react()],
-})
+});
